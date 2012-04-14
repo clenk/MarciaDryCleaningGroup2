d9bd3a6c4d0f7b873238a38463ed9d23640f548b
AA++ <Administrator@cscl-checkout01.(none)>
2012-04-13 22:47:34 -0400

    set the schema to be created to be mdc instead of drycleaning

--- create.sql
+++ create.sql
@@ -2,8 +2,8 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-CREATE SCHEMA IF NOT EXISTS `drycleaning` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
-USE `drycleaning` ;
+CREATE SCHEMA IF NOT EXISTS `mdc` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `mdc` ;
 
 -- -----------------------------------------------------
 -- Table `drycleaning`.`CUSTOMER_DATA`
