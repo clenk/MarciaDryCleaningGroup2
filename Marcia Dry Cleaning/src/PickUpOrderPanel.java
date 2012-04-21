fc44760c937b3b0bedf972430e11fd18136d6628
sehcheese <sehcheese@gmail.com>
2012-04-21 12:15:26 -0400

    some actionlisteners for pick up order panel, namely the pick up order submit and the order id lookup, waiting on arthur to take his code for the other three lookups

--- Marcia Dry Cleaning/src/PickUpOrderPanel.java
+++ Marcia Dry Cleaning/src/PickUpOrderPanel.java
@@ -3,11 +3,21 @@
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -27,6 +37,8 @@ public class PickUpOrderPanel
 	private JTextField lastName = new JTextField(15);
 	private JTextField telephone = new JTextField(15);
 	private JTextField orderID = new JTextField(15);
+	private JComboBox paymentOptions = new JComboBox();
+	private int activeOrderID = 0;
 	
 	public PickUpOrderPanel(Connection conn)
 	{
@@ -67,6 +79,11 @@ public class PickUpOrderPanel
 		searchPanel.add(orderID);
 		searchPanel.add(orderIDSubmit);
 		
+		firstNameSubmit.addActionListener(new firstNameListener());
+		lastNameSubmit.addActionListener(new lastNameListener());
+		telephoneSubmit.addActionListener(new telephoneListener());
+		orderIDSubmit.addActionListener(new orderIDListener());
+		
 		PickUpOrderPanel.add(searchPanel, BorderLayout.NORTH);
 	}
 		
@@ -94,8 +111,137 @@ public class PickUpOrderPanel
 	
 	public void buildConfirmPanel()
 	{
-		confirmPanel.add(new JLabel("jfdkslaa;jfdsal;"));
+		Border selectPaymentOptionsBorder = BorderFactory.createTitledBorder("Pick Up Order:");
+		confirmPanel.setBorder(selectPaymentOptionsBorder);
+		
+		JPanel selectPaymentOption = new JPanel();
+		paymentOptions.addItem("Cash");
+		paymentOptions.addItem("Check");
+		paymentOptions.addItem("Visa");
+		paymentOptions.addItem("MasterCard");
+		paymentOptions.addItem("Discover");
+		selectPaymentOption.add(paymentOptions);
+		confirmPanel.add(selectPaymentOption, BorderLayout.WEST);
+		
+		JPanel submitPickUpOrder = new JPanel();
+		JButton submitPickUp = new JButton("Submit Pick Up");
+		submitPickUpOrder.add(submitPickUp);
+		confirmPanel.add(submitPickUpOrder, BorderLayout.EAST);
+		
+		submitPickUp.addActionListener(new submitPickUpListener());
+		
 		PickUpOrderPanel.add(confirmPanel, BorderLayout.SOUTH);
 	}
-
+	
+	// LISTENERS for the PickUpOrderPanel
+	
+	// FIRST NAME search listener
+	private class firstNameListener implements ActionListener 
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			
+		}
+	}
+	
+	// LAST NAME search listener
+	private class lastNameListener implements ActionListener 
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			
+		}
+	}
+	
+	// TELEPHONE search listener
+	private class telephoneListener implements ActionListener 
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			
+		}
+	}
+	
+	// ORDER ID search listener
+	private class orderIDListener implements ActionListener 
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			dispTA.setText("");
+			
+			try 
+			{
+				int orderNumber = Integer.parseInt(orderID.getText());
+				PreparedStatement orderIDLookup = conn.prepareStatement("SELECT * FROM order_data WHERE OrderNumber = ?");
+				orderIDLookup.setInt(1, orderNumber);
+				ResultSet orderIDrs = orderIDLookup.executeQuery();
+				orderIDrs.next();
+				
+				activeOrderID = orderIDrs.getInt("OrderNumber");
+				
+				StringBuilder sb = new StringBuilder();
+				sb.append("Order ID: ");
+				sb.append(orderNumber);
+				sb.append("\n\nDate Dropped Off: ");
+				sb.append(orderIDrs.getString("DateDroppedOff"));
+				sb.append("\nDate Promised: ");
+				sb.append(orderIDrs.getString("DatePromised"));
+				sb.append("\nDate Picked Up: ");
+				sb.append(orderIDrs.getString("DatePickedUp"));
+				sb.append("\n\nPrice: ");
+				sb.append(orderIDrs.getString("Price"));
+				sb.append("\nTax: ");
+				sb.append(orderIDrs.getString("Tax"));
+				sb.append("\nTotal: ");
+				sb.append(orderIDrs.getString("Total"));
+				sb.append("\n\nPayment Method: ");
+				sb.append(orderIDrs.getString("PaymentMethod"));
+				
+				dispTA.append(sb.toString());				
+			} 
+			catch (SQLException e1) 
+			{
+				JOptionPane.showMessageDialog(null, "Enter a valid Order ID!");
+				return;
+			}
+			catch(NumberFormatException e1)
+			{
+				JOptionPane.showMessageDialog(null, "Enter a valid Order ID!");
+				return;
+			}
+		}
+	}
+	private class submitPickUpListener implements ActionListener 
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			if(activeOrderID==0)
+			{
+				JOptionPane.showMessageDialog(null, "First, look up an order.");
+				return;
+			}
+			
+			try 
+			{
+				PreparedStatement pickUpOrderSubmit = conn.prepareStatement("UPDATE order_data SET PaymentMethod = ?, DatePickedUp = ? WHERE OrderNumber = ?");
+				String paymentMethod = (String) paymentOptions.getSelectedItem();
+				pickUpOrderSubmit.setString(1, paymentMethod);
+				
+				Date currentDatetime = new Date(System.currentTimeMillis());
+				Timestamp timestamp = new Timestamp(currentDatetime.getTime());
+				pickUpOrderSubmit.setTimestamp(2, timestamp);
+				
+				pickUpOrderSubmit.setInt(3, activeOrderID);
+				
+				pickUpOrderSubmit.executeUpdate();
+				JOptionPane.showMessageDialog(null, "Order picked up: " + timestamp);
+			} 
+			catch (SQLException e1) 
+			{
+				e1.printStackTrace();
+			}
+			
+		}
+		
+	}
 }
