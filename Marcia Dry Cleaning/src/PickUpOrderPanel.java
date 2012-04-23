0227cba780f85d83d67737fec2d776c95fea5487
sehcheese <sehcheese@gmail.com>
2012-04-23 00:49:31 -0400

    finish pickuporderpanel

--- Marcia Dry Cleaning/src/PickUpOrderPanel.java
+++ Marcia Dry Cleaning/src/PickUpOrderPanel.java
@@ -10,6 +10,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -39,6 +40,11 @@ public class PickUpOrderPanel
 	private JTextField orderID = new JTextField(15);
 	private JComboBox paymentOptions = new JComboBox();
 	private int activeOrderID = 0;
+	private ArrayList<String> orders = new ArrayList<String>();
+	private ArrayList<Integer> activeOrderIDIndices = new ArrayList<Integer>();
+	private int entriesIndex;
+	private BasicArrowButton leftBtn;
+	private BasicArrowButton rightBtn;
 	
 	public PickUpOrderPanel(Connection conn)
 	{
@@ -62,26 +68,22 @@ public class PickUpOrderPanel
 		JLabel lastNameTag = new JLabel("Last Name:");
 		JLabel telephoneTag = new JLabel("Telephone:");
 		JLabel orderIDTag = new JLabel("Order ID:");
-		JButton firstNameSubmit = new JButton("Submit");
-		JButton lastNameSubmit = new JButton("Submit");
-		JButton telephoneSubmit = new JButton("Submit");
+		JButton personSubmit = new JButton("Submit");
 		JButton orderIDSubmit = new JButton("Submit");
 		searchPanel.add(firstNameTag);
 		searchPanel.add(firstName);
-		searchPanel.add(firstNameSubmit);
+		searchPanel.add(new JLabel(""));
 		searchPanel.add(lastNameTag);
 		searchPanel.add(lastName);
-		searchPanel.add(lastNameSubmit);
+		searchPanel.add(new JLabel(""));
 		searchPanel.add(telephoneTag);
 		searchPanel.add(telephone);
-		searchPanel.add(telephoneSubmit);
+		searchPanel.add(personSubmit);
 		searchPanel.add(orderIDTag);
 		searchPanel.add(orderID);
 		searchPanel.add(orderIDSubmit);
 		
-		firstNameSubmit.addActionListener(new firstNameListener());
-		lastNameSubmit.addActionListener(new lastNameListener());
-		telephoneSubmit.addActionListener(new telephoneListener());
+		personSubmit.addActionListener(new nameListener());
 		orderIDSubmit.addActionListener(new orderIDListener());
 		
 		PickUpOrderPanel.add(searchPanel, BorderLayout.NORTH);
@@ -96,11 +98,11 @@ public class PickUpOrderPanel
 		dispTA.enableInputMethods(false);
 		dispTA.setEditable(false);
 		JPanel bp = new JPanel();
-		BasicArrowButton leftBtn = new BasicArrowButton(SwingConstants.WEST);
-		//leftBtn.addActionListener(new LeftListener());
+		leftBtn = new BasicArrowButton(SwingConstants.WEST);
+		leftBtn.addActionListener(new leftListener());
 		bp.add(leftBtn, BorderLayout.WEST);
-		BasicArrowButton rightBtn = new BasicArrowButton(SwingConstants.EAST);
-		//rightBtn.addActionListener(new RightListener());
+		rightBtn = new BasicArrowButton(SwingConstants.EAST);
+		rightBtn.addActionListener(new rightListener());
 		bp.add(rightBtn, BorderLayout.EAST);
 		displayPanel.add(dispTA, BorderLayout.NORTH);
 		displayPanel.add(bp);
@@ -134,30 +136,142 @@ public class PickUpOrderPanel
 	
 	// LISTENERS for the PickUpOrderPanel
 	
-	// FIRST NAME search listener
-	private class firstNameListener implements ActionListener 
+	// FIRST NAME, LAST NAME, TELEPHONE search listener
+	private class nameListener implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e) 
 		{
+			dispTA.setText("");
+			orders.clear();
+			activeOrderIDIndices.clear();
+			rightBtn.setEnabled(true);
+			leftBtn.setEnabled(true);
 			
+			try
+			{
+				if(!firstName.getText().equals("") && !lastName.getText().equals("")) 
+				{
+					PreparedStatement nameQuery = conn.prepareStatement("SELECT * FROM CUSTOMER_DATA WHERE FIRST = ? and LAST = ?");
+					String first = firstName.getText();
+					String last = lastName.getText();
+					nameQuery.setString(1, first);
+					nameQuery.setString(2, last);
+					ResultSet rs = nameQuery.executeQuery();
+					rs.next();
+					int customerID = rs.getInt("idCustomer");
+					PreparedStatement orderQueryByID = conn.prepareStatement("SELECT * FROM ORDER_DATA WHERE CUSTOMER_DATA_idCustomer = ? and DatePickedUp is null;");
+					orderQueryByID.setInt(1, customerID);
+					ResultSet orderIDrs = orderQueryByID.executeQuery();
+					while(orderIDrs.next())
+					{
+						activeOrderID = orderIDrs.getInt("OrderNumber");
+						
+						StringBuilder sb = new StringBuilder();
+						sb.append("Order ID: ");
+						sb.append(activeOrderID);
+						sb.append("\n\nDate Dropped Off: ");
+						sb.append(orderIDrs.getString("DateDroppedOff"));
+						sb.append("\nDate Promised: ");
+						sb.append(orderIDrs.getString("DatePromised"));
+						sb.append("\nDate Picked Up: ");
+						sb.append(orderIDrs.getString("DatePickedUp"));
+						sb.append("\n\nPrice: ");
+						sb.append(orderIDrs.getString("Price"));
+						sb.append("\nTax: ");
+						sb.append(orderIDrs.getString("Tax"));
+						sb.append("\nTotal: ");
+						sb.append(orderIDrs.getString("Total"));
+						sb.append("\n\nPayment Method: ");
+						sb.append(orderIDrs.getString("PaymentMethod"));
+						orders.add(sb.toString());
+						activeOrderIDIndices.add(activeOrderID);
+					}
+					if(orders.size()!=0)
+					{
+						dispTA.setText(orders.get(0));
+						entriesIndex = 0;
+					}
+				} 
+				else if(!telephone.equals(null))
+				{
+					PreparedStatement telephoneQuery = conn.prepareStatement("select idCustomer from customer_data, customer_data_has_phone, phone where customer_data.idCustomer = customer_data_has_phone.CUSTOMER_DATA_idCustomer && customer_data_has_phone.PHONE_idPhone = phone.idPhone && phone.phoneNum = ?");
+					telephoneQuery.setString(1,telephone.getText());
+					ResultSet custID = telephoneQuery.executeQuery();
+					custID.next();
+					int customerID = custID.getInt("idCustomer");
+					
+					PreparedStatement orderQueryByID = conn.prepareStatement("SELECT * FROM ORDER_DATA WHERE CUSTOMER_DATA_idCustomer = ? and DatePickedUp is null;");
+					orderQueryByID.setInt(1, customerID);
+					ResultSet orderIDrs = orderQueryByID.executeQuery();
+					while(orderIDrs.next())
+					{
+						activeOrderID = orderIDrs.getInt("OrderNumber");
+						
+						StringBuilder sb = new StringBuilder();
+						sb.append("Order ID: ");
+						sb.append(activeOrderID);
+						sb.append("\n\nDate Dropped Off: ");
+						sb.append(orderIDrs.getString("DateDroppedOff"));
+						sb.append("\nDate Promised: ");
+						sb.append(orderIDrs.getString("DatePromised"));
+						sb.append("\nDate Picked Up: ");
+						sb.append(orderIDrs.getString("DatePickedUp"));
+						sb.append("\n\nPrice: ");
+						sb.append(orderIDrs.getString("Price"));
+						sb.append("\nTax: ");
+						sb.append(orderIDrs.getString("Tax"));
+						sb.append("\nTotal: ");
+						sb.append(orderIDrs.getString("Total"));
+						sb.append("\n\nPayment Method: ");
+						sb.append(orderIDrs.getString("PaymentMethod"));
+						orders.add(sb.toString());
+						activeOrderIDIndices.add(activeOrderID);
+					}
+					if(orders.size()!=0)
+					{
+						dispTA.setText(orders.get(0));
+						entriesIndex = 0;
+					}
+				} 
+				else 
+				{
+					JOptionPane.showMessageDialog(null, "You must have first & last names OR phone number");
+				}
+			} 
+			catch (SQLException e1)
+			{
+				JOptionPane.showMessageDialog(null, "Invalid arguments.");
+				e1.printStackTrace();
+				return;
+			}
 		}
 	}
 	
-	// LAST NAME search listener
-	private class lastNameListener implements ActionListener 
+	// LEFT LISTENER
+	private class leftListener implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e) 
 		{
-			
+			if(entriesIndex-1>-1)
+			{
+				entriesIndex = entriesIndex-1;
+				dispTA.setText(orders.get(entriesIndex));
+				activeOrderID = activeOrderIDIndices.get(entriesIndex);
+			}
 		}
 	}
 	
-	// TELEPHONE search listener
-	private class telephoneListener implements ActionListener 
+	//RIGHT LISTENER
+	private class rightListener implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent e) 
 		{
-			
+			if(entriesIndex+1<orders.size())
+			{
+				entriesIndex = entriesIndex+1;
+				dispTA.setText(orders.get(entriesIndex));
+				activeOrderID = activeOrderIDIndices.get(entriesIndex);
+			}
 		}
 	}
 	
@@ -167,6 +281,8 @@ public class PickUpOrderPanel
 		public void actionPerformed(ActionEvent e) 
 		{
 			dispTA.setText("");
+			leftBtn.setEnabled(true);
+			rightBtn.setEnabled(true);
 			
 			try 
 			{
@@ -219,36 +335,48 @@ public class PickUpOrderPanel
 				JOptionPane.showMessageDialog(null, "First, look up an order.");
 				return;
 			}
-			
+			PreparedStatement checkIfPickedUp;
 			try 
 			{
-				PreparedStatement checkIfPickedUp = conn.prepareStatement("SELECT * FROM order_data WHERE OrderNumber = ?");
+				checkIfPickedUp = conn.prepareStatement("SELECT * FROM order_data WHERE OrderNumber = ?");
 				checkIfPickedUp.setInt(1, activeOrderID);
 				ResultSet order = checkIfPickedUp.executeQuery();
 				order.next();
-				if(!order.getString("DatePickedUp").equals("null"))
+				if(order.getString("DatePickedUp")!=null)
 				{
 					JOptionPane.showMessageDialog(null, "Order already picked up!");
 					return;
 				}
-				
-				PreparedStatement pickUpOrderSubmit = conn.prepareStatement("UPDATE order_data SET PaymentMethod = ?, DatePickedUp = ? WHERE OrderNumber = ?");
-				String paymentMethod = (String) paymentOptions.getSelectedItem();
-				pickUpOrderSubmit.setString(1, paymentMethod);
-				
-				Date currentDatetime = new Date(System.currentTimeMillis());
-				Timestamp timestamp = new Timestamp(currentDatetime.getTime());
-				pickUpOrderSubmit.setTimestamp(2, timestamp);
-				
-				pickUpOrderSubmit.setInt(3, activeOrderID);
-				
-				pickUpOrderSubmit.executeUpdate();
-				JOptionPane.showMessageDialog(null, "Order picked up: " + timestamp);
+				else
+				{
+					try 
+					{				
+						PreparedStatement pickUpOrderSubmit = conn.prepareStatement("UPDATE order_data SET PaymentMethod = ?, DatePickedUp = ? WHERE OrderNumber = ?");
+						String paymentMethod = (String) paymentOptions.getSelectedItem();
+						pickUpOrderSubmit.setString(1, paymentMethod);
+						
+						Date currentDatetime = new Date(System.currentTimeMillis());
+						Timestamp timestamp = new Timestamp(currentDatetime.getTime());
+						pickUpOrderSubmit.setTimestamp(2, timestamp);
+						
+						pickUpOrderSubmit.setInt(3, activeOrderID);
+						
+						pickUpOrderSubmit.executeUpdate();
+						JOptionPane.showMessageDialog(null, "Order picked up: " + timestamp);
+						leftBtn.setEnabled(false);
+						rightBtn.setEnabled(false);
+					} 
+					catch (SQLException e1) 
+					{
+						e1.printStackTrace();
+					}
+				}
 			} 
-			catch (SQLException e1) 
+			catch (SQLException e2) 
 			{
-				e1.printStackTrace();
+				e2.printStackTrace();
 			}
+			
 			dispTA.setText("");
 			
 			try 
